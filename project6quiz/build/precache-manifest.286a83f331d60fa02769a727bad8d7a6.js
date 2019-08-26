428f6e1a3910cf0ebae0abab4ccd812c41a735e3
mike <mpakleung@gmail.com>
2019-08-25 23:46:38 -0400

    Add the build for project6

---
+++ project6quiz/build/precache-manifest.286a83f331d60fa02769a727bad8d7a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b2cc22e3e2c7160f1dc40ec4360c7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd18c20f5b9bb4a74422",
+    "url": "/static/css/main.87748316.chunk.css"
+  },
+  {
+    "revision": "f9ae0318136e4311ea91",
+    "url": "/static/js/2.93e92daa.chunk.js"
+  },
+  {
+    "revision": "fd18c20f5b9bb4a74422",
+    "url": "/static/js/main.0c4116f7.chunk.js"
+  },
+  {
+    "revision": "05b8671feef85fcac9a0",
+    "url": "/static/js/runtime~main.142fa00d.js"
+  }
+]);
