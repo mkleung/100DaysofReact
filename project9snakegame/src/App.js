8b6b9cdb069a799b539ca0bc7c15cc20ae13e84b
Mike <mpakleung@gmail.com>
2019-09-03 17:27:07 -0400

    Complete project 9

--- project9snakegame/src/App.js
+++ project9snakegame/src/App.js
@@ -11,19 +11,28 @@ const getRandomCoordinates = () => {
   return [x, y]
 }
 
+const initialState = {
+  food: getRandomCoordinates(),
+  speed: 200,
+  direction: "RIGHT",
+  snakeDots: [
+    [0, 0],
+    [2, 0]
+  ]
+}
 class App extends Component {
 
-  state = {
-    food: getRandomCoordinates(),
-    direction: "RIGHT",
-    snakeDots: [
-      [0, 0],
-      [2, 0]
-    ]
-  }
+  state = initialState;
 
   componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed)
     document.onkeydown = this.onKeyDown;
+  }
+
+  componentDidUpdate() {
+    this.checkIfOutofBorders();
+    this.checkIfCollapsed();
+    this.checkIfEat();
   }
 
   onKeyDown = (e) => {
@@ -43,6 +52,88 @@ class App extends Component {
         this.setState({ direction: 'RIGHT' });
         break;
     }
+  }
+
+  moveSnake = () => {
+    let dots = [...this.state.snakeDots];
+    let head = dots[dots.length - 1];
+
+    switch (this.state.direction) {
+      case "RIGHT":
+        head = [head[0] + 2, head[1]];
+        break;
+        case "LEFT":
+            head = [head[0] - 2, head[1]];
+            break;
+        case "DOWN":
+            head = [head[0], head[1] + 2];
+            break;
+        case "UP":
+            head = [head[0], head[1] - 2];
+            break;
+    }
+
+    dots.push(head)
+    dots.shift();
+
+    this.setState({
+      snakeDots: dots
+    })
+  }
+
+  checkIfOutofBorders() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+
+    if (head[0] >= 100 || head[1] == 100 || head[0] < 0 || head[1] < 0){
+      this.onGameOver();
+    }
+  }
+
+  checkIfEat() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    let food = this.state.food;
+    if (head[0] === food[0] || head[1] === food[1]){
+      this.setState({
+        food: getRandomCoordinates()
+      })
+      this.enlargeSnake();
+      this.increaseSpeed();
+    }
+  }
+
+  enlargeSnake() {
+    let newSnake = [...this.state.snakeDots];
+    newSnake.unshift([]);
+    this.setState({
+      snakeDots: newSnake
+    })
+  }
+
+  increaseSpeed() {
+    if (this.state.speed > 10) {
+      this.setState({
+        speed: this.state.speed - 10
+      })
+    }
+  }
+
+  checkIfCollapsed() {
+    let snake = [...this.state.snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop();
+    snake.forEach(dot => {
+      if (head[0] == dot[0] && head[1] == dot[1]) {
+        this.onGameOver();
+      }
+    })
+  }
+
+  onGameOver() {
+    alert(`Game over!. The snake length is ${this.state.snakeDots.length}`)
+  
+    this.setState(
+      initialState
+    )
   }
 
   render() {
